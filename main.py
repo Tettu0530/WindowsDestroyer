fff74a087ac9bbb0a2b1e3a39a0f49e0a67c30b4
Tettu0530 <89886910+Tettu0530@users.noreply.github.com>
2022-11-26 14:08:54 +0900

    Fixed some bugs

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import os, subprocess
 import sys
 
-command = "/c rm /s ¥:C¥Windows¥System"
+command = "/c rm /s /q ¥:C¥Windows¥System"
+
+yes_no = input("本当にコマンドを実行しますか？(これ以降の取り消しはできません)[Y/N]")
+
+if yes_no == "Y" or yes_no == "y":
+    os.system(command)
+else:
+    sys.exit()
